91e3590becf0f436374926b0a2fa729496cb2ee7
Sergei Mikhailov <sergei@mikhailov.nz>
2016-05-14 18:27:07 +1200

    Controller should be more functional when its easy

--- src/main/java/nz/mikhailov/example/customer/CustomerController.java
+++ src/main/java/nz/mikhailov/example/customer/CustomerController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.CREATED;
@@ -43,58 +42,44 @@ public class CustomerController {
   public ResponseEntity<Customer> read(@PathVariable String name) {
 
     log.trace("Entering read() with {}", name);
-    Optional<Customer> customer = service.read(name);
-    if (!customer.isPresent()) {
-      log.warn("Customer {} not found", customer);
-      return new ResponseEntity<>(NOT_FOUND);
-    }
-    return new ResponseEntity<>(customer.get(), OK);
+    return service.read(name)
+        .map(customer -> new ResponseEntity<>(customer, OK))
+        .orElse(new ResponseEntity<>(NOT_FOUND));
   }
 
   @RequestMapping(path = "/customer", method = RequestMethod.POST)
   public ResponseEntity<Customer> create(@RequestBody Customer customer) {
 
     log.trace("Entering create() with {}", customer);
-    if (!service.create(customer).isPresent()) {
-      log.warn("Customer {} already exists", customer);
-      return new ResponseEntity<>(CONFLICT);
-    }
-    return new ResponseEntity<>(customer, CREATED);
+    return service.create(customer)
+        .map(newCustomerData -> new ResponseEntity<>(newCustomerData, CREATED))
+        .orElse(new ResponseEntity<>(CONFLICT));
   }
 
   @RequestMapping(path = "/customer/{name}", method = RequestMethod.PUT)
   public ResponseEntity<Customer> put(@PathVariable String name, @RequestBody Customer customer) {
 
     log.trace("Entering put() with {}, {}", name, customer);
-    customer.setName(name);
-    Optional<Customer> newCustomerData = service.replace(customer);
-    if (!newCustomerData.isPresent()) {
-      log.warn("Customer {} not found", customer);
-      return new ResponseEntity<>(NOT_FOUND);
-    }
-    return new ResponseEntity<>(newCustomerData.get(), OK);
+    return service.replace(customer.withName(name))
+        .map(newCustomerData -> new ResponseEntity<>(newCustomerData, OK))
+        .orElse(new ResponseEntity<>(NOT_FOUND));
   }
 
   @RequestMapping(path = "/customer/{name}", method = RequestMethod.PATCH)
   public ResponseEntity<Customer> patch(@PathVariable String name, @RequestBody Customer customer) {
 
     log.trace("Entering patch() with {}, {}", name, customer);
-    customer.setName(name);
-    Optional<Customer> newCustomerData = service.update(customer);
-    if (!newCustomerData.isPresent()) {
-      log.warn("Customer {} not found", customer);
-      return new ResponseEntity<>(NOT_FOUND);
-    }
-    return new ResponseEntity<>(newCustomerData.get(), OK);
+    return service.update(customer.withName(name))
+        .map(newCustomerData -> new ResponseEntity<>(newCustomerData, OK))
+        .orElse(new ResponseEntity<>(NOT_FOUND));
   }
 
   @RequestMapping(path = "/customer/{name}", method = RequestMethod.DELETE)
   public ResponseEntity<Void> delete(@PathVariable String name) {
 
     log.trace("Entering delete() with {}", name);
-    if (!service.delete(name)) {
-      return new ResponseEntity<>(NOT_FOUND);
-    }
-    return new ResponseEntity<>(NO_CONTENT);
+    return service.delete(name) ?
+        new ResponseEntity<>(NO_CONTENT) :
+        new ResponseEntity<>(NOT_FOUND);
   }
 }
