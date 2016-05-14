b33b295f54a01f907af3c0af4ed223eb658487aa
Sergei Mikhailov <sergei@mikhailov.nz>
2016-05-14 13:54:30 +1200

    Application should connect to AWS DynamoDB
    By default it's connected to dynamodb-local implementation.
    Can be overriden by system property "dynamoDbEndpoint"
    
    Download dynamodb-local from here http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.html#DynamoDBLocal.DownloadingAndRunning

--- src/main/java/nz/mikhailov/example/ApplicationConfiguration.java
+++ src/main/java/nz/mikhailov/example/ApplicationConfiguration.java
@@ -1,8 +1,31 @@
 package nz.mikhailov.example;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfiguration {
+
+  private final static String DYNAMODB_ENDPOINT_DEFAULT_VALUE = "http://localhost:8000";
+
+  private final Logger log = LoggerFactory.getLogger(getClass());
+
+  @Value("${dynamoDbEndpoint:" + DYNAMODB_ENDPOINT_DEFAULT_VALUE + "}")
+  private String dynamoDbEndpoint;
+
+  @Bean
+  public AmazonDynamoDB amazonDynamoDb() {
+
+    log.trace("Entering amazonDynamoDb()");
+    AmazonDynamoDB client = new AmazonDynamoDBClient();
+    log.info("Using DynamoDb endpoint {}", dynamoDbEndpoint);
+    client.setEndpoint(dynamoDbEndpoint);
+    return client;
+  }
 
 }
