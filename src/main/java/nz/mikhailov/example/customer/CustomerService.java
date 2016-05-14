480bf02c2bd652bf7889a9464c261aba2cbeb20e
Sergei Mikhailov <sergei@mikhailov.nz>
2016-05-14 16:07:04 +1200

    Shoud have service to support CRUD operations

--- src/main/java/nz/mikhailov/example/customer/CustomerService.java
+++ src/main/java/nz/mikhailov/example/customer/CustomerService.java
@@ -2,11 +2,80 @@ package nz.mikhailov.example.customer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static com.amazonaws.util.StringUtils.isNullOrEmpty;
 
 @Service
 public class CustomerService {
 
   private final Logger log = LoggerFactory.getLogger(getClass());
 
+  @Autowired
+  private CustomerRepository repository;
+
+  public Optional<Customer> read(String name) {
+
+    log.trace("Entering read() with {}", name);
+    return repository.read(name);
+  }
+
+  public Optional<Customer> create(Customer customer) {
+
+    log.trace("Entering create() with {}", customer);
+    if (repository.read(customer.getName()).isPresent()) {
+      log.warn("Customer {} not found", customer.getName());
+      return Optional.empty();
+    }
+    repository.save(customer);
+    return Optional.of(customer);
+  }
+
+  public Optional<Customer> replace(Customer newCustomerData) {
+
+    log.trace("Entering replace() with {}", newCustomerData);
+    Optional<Customer> existingCustomer = repository.read(newCustomerData.getName());
+    if (!existingCustomer.isPresent()) {
+      log.warn("Customer {} not found", newCustomerData.getName());
+      return Optional.empty();
+    }
+    Customer customer = existingCustomer.get();
+    customer.setAddress(newCustomerData.getAddress());
+    customer.setPhoneNumber(newCustomerData.getPhoneNumber());
+    repository.save(customer);
+    return Optional.of(customer);
+  }
+
+  public Optional<Customer> update(Customer newCustomerData) {
+
+    log.trace("Entering update() with {}", newCustomerData);
+    Optional<Customer> existingCustomer = repository.read(newCustomerData.getName());
+    if (!existingCustomer.isPresent()) {
+      log.warn("Customer {} not found", newCustomerData.getName());
+      return Optional.empty();
+    }
+    Customer customer = existingCustomer.get();
+    if (!isNullOrEmpty(newCustomerData.getAddress())) {
+      customer.setAddress(newCustomerData.getAddress());
+    }
+    if (!isNullOrEmpty(newCustomerData.getPhoneNumber())) {
+      customer.setPhoneNumber(newCustomerData.getPhoneNumber());
+    }
+    repository.save(customer);
+    return Optional.of(customer);
+  }
+
+  public boolean delete(String name) {
+
+    log.trace("Entering delete() with {}", name);
+    if (!repository.read(name).isPresent()) {
+      log.warn("Customer {} not found", name);
+      return false;
+    }
+    repository.delete(name);
+    return true;
+  }
 }
---
+++ src/test/java/nz/mikhailov/example/customer/CustomerServiceTest.java
@@ -0,0 +1,133 @@
+package nz.mikhailov.example.customer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Optional;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CustomerServiceTest {
+
+  @Mock
+  private CustomerRepository repository;
+
+  @InjectMocks
+  private CustomerService service;
+
+  @Test
+  public void readShouldReturnEmptyOptionalWhenNoCustomerFound() throws Exception {
+
+    when(repository.read("Arthur C. Clarke")).thenReturn(Optional.empty());
+    Optional<Customer> result = service.read("Arthur C. Clarke");
+    assertThat(result, is(Optional.empty()));
+  }
+
+  @Test
+  public void readShouldReturnResultWhenCustomerFound() throws Exception {
+
+    Customer customer = new Customer().withName("Arthur C. Clarke");
+    when(repository.read("Arthur C. Clarke")).thenReturn(Optional.of(customer));
+    Customer result = service.read("Arthur C. Clarke").get();
+    assertThat(result, is(equalTo(customer)));
+  }
+
+  @Test
+  public void createShouldReturnEmptyOptionalWhenCustomerAlreadyExists() throws Exception {
+
+    Customer existingCustomer = new Customer().withName("Arthur C. Clarke").withAddress("Sri Lanka");
+    when(repository.read("Arthur C. Clarke")).thenReturn(Optional.of(existingCustomer));
+    Customer newCustomer = new Customer().withName("Arthur C. Clarke");
+    Optional<Customer> result = service.create(newCustomer);
+    assertThat(result, is(Optional.empty()));
+    verify(repository, never()).save(newCustomer);
+  }
+
+  @Test
+  public void createShouldReturnNewCustomerWhenCustomerNotYetExists() throws Exception {
+
+    Customer newCustomer = new Customer().withName("Arthur C. Clarke");
+    when(repository.read("Arthur C. Clarke")).thenReturn(Optional.empty());
+    Customer result = service.create(newCustomer).get();
+    assertThat(result, is(equalTo(newCustomer)));
+    verify(repository).save(newCustomer);
+  }
+
+  @Test
+  public void replaceShouldReturnEmptyOptionalWhenCustomerNotFound() throws Exception {
+
+    Customer newCustomerData = new Customer().withName("Arthur C. Clarke").withAddress("Sri Lanka");
+    when(repository.read("Arthur C. Clarke")).thenReturn(Optional.empty());
+    Optional<Customer> result = service.replace(newCustomerData);
+    assertThat(result, is(Optional.empty()));
+    verify(repository, never()).save(newCustomerData);
+  }
+
+  @Test
+  public void replaceShouldOverwriteAndReturnNewDataWhenCustomerExists() throws Exception {
+
+    Customer oldCustomerData = new Customer().withName("Arthur C. Clarke").withPhoneNumber("000000");
+    Customer newCustomerData = new Customer().withName("Arthur C. Clarke").withAddress("Sri Lanka");
+    when(repository.read("Arthur C. Clarke")).thenReturn(Optional.of(oldCustomerData));
+    Customer result = service.replace(newCustomerData).get();
+    assertThat(result, is(equalTo(newCustomerData)));
+    verify(repository).save(newCustomerData);
+  }
+
+  @Test
+  public void updateShouldReturnEmptyOptionalWhenCustomerNotFound() throws Exception {
+
+    Customer newCustomerData = new Customer().withName("Arthur C. Clarke").withAddress("Sri Lanka");
+    when(repository.read("Arthur C. Clarke")).thenReturn(Optional.empty());
+    Optional<Customer> result = service.update(newCustomerData);
+    assertThat(result, is(Optional.empty()));
+    verify(repository, never()).save(newCustomerData);
+  }
+
+  @Test
+  public void updateShouldOverwriteExistingFieldAndReturnNewDataWhenCustomerExists() throws Exception {
+
+    Customer oldCustomerData = new Customer().withName("Arthur C. Clarke").withAddress("England");
+    Customer newCustomerData = new Customer().withName("Arthur C. Clarke").withAddress("Sri Lanka");
+    when(repository.read("Arthur C. Clarke")).thenReturn(Optional.of(oldCustomerData));
+    Customer result = service.update(newCustomerData).get();
+    assertThat(result, is(equalTo(newCustomerData)));
+    verify(repository).save(newCustomerData);
+  }
+
+  @Test
+  public void updateShouldNotOverwriteExistingFieldIfNoNewValuePassedAndShouldReturnNewDataWhenCustomerExists() throws Exception {
+
+    Customer oldCustomerData = new Customer().withName("Arthur C. Clarke").withAddress("England");
+    Customer newCustomerData = new Customer().withName("Arthur C. Clarke").withPhoneNumber("000000");
+    Customer expectedResult = new Customer().withName("Arthur C. Clarke").withAddress("England").withPhoneNumber("000000");
+    when(repository.read("Arthur C. Clarke")).thenReturn(Optional.of(oldCustomerData));
+    Customer result = service.update(newCustomerData).get();
+    assertThat(result, is(equalTo(expectedResult)));
+    verify(repository).save(expectedResult);
+  }
+
+  @Test
+  public void deleteShouldReturnFalseWhenCustomerNotFound() throws Exception {
+
+    when(repository.read("Arthur C. Clarke")).thenReturn(Optional.empty());
+    boolean result = service.delete("Arthur C. Clarke");
+    assertThat(result, is(false));
+  }
+
+  @Test
+  public void deleteShouldReturnTrueWhenCustomerDeleted() throws Exception {
+
+    when(repository.read("Arthur C. Clarke")).thenReturn(Optional.of(new Customer().withName("Arthur C. Clarke")));
+    boolean result = service.delete("Arthur C. Clarke");
+    assertThat(result, is(true));
+    verify(repository).delete("Arthur C. Clarke");
+  }
+}
